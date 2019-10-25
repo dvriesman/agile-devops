390abc3873ef76a9f3420db26e2e6e1d195fd3f4
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-10-25 10:49:41 -0300

    Update install_vagrant_centos.sh

--- install_curso_devops/install_vagrant_centos.sh
+++ install_curso_devops/install_vagrant_centos.sh
@@ -4,7 +4,7 @@ yum -y install qemu-kvm libvirt virt-install bridge-utils libvirt-devel  libxslt
 echo "net.ipv4.ip_forward = 1"|sudo tee /etc/sysctl.d/99-ipforward.conf
 sysctl -p /etc/sysctl.d/99-ipforward.conf
 systemctl start libvirtd
-systemctl enabled libvirtd
+systemctl enable libvirtd
 systemctl start libvirtd
 #curl -o vagrant.rpm https://releases.hashicorp.com/vagrant/1.7.4/vagrant_1.7.4_x86_64.rpm
 curl -o vagrant.rpm https://releases.hashicorp.com/vagrant/2.2.6/vagrant_2.2.6_x86_64.rpm
