49e48d6494f62ffec3b7d437713768c1c69b2a3f
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-10-29 20:49:40 -0300

    melhora a aplicação

--- week1/apps/colored/color.py
+++ week1/apps/colored/color.py
@@ -7,13 +7,14 @@ import os
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
+name = os.environ.get("APP_NAME")
 color = os.environ.get('APP_COLOR')
 
 @app.route('/')
 def root():
     app.logger.info('No metodo root')
 
-    return render_template('hello.html', color=color)
+    return render_template('hello.html', color=color, name=name)
     
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=9090)
